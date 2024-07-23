3af1139e3139c07ebaff9e43adbd6a9f810ea733
Tori Qiu <qiutori@gmail.com>
2024-07-23 15:20:54 +0800

    Bootable disk image + VGA text interace

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 #![no_std]
-#![no_main] // Don't use the normal entry point chain
+#![no_main] // Don't use the normal entry point
+mod vga_buffer;
 
 use core::panic::PanicInfo;
 
-// PanicInfo contains file and line where the panic happened 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-fn main() {}
+// static START: &[u8] = b"Starting!";
+
+#[no_mangle]
+// Overwrite OS entry point
+pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
+
+    loop {}
+}
---
+++ src/vga_buffer.rs
@@ -0,0 +1,107 @@
+#[allow(dead_code)]
+// Enable copy semantics for the type 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+// Guarantee field ordering in structs
+#[repr(C)]
+pub enum Color {
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    Pink = 13,
+    Yellow = 14,
+    White = 15,
+}
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+struct ColorCode(u8);
+
+// Rust has no u4 type
+impl ColorCode {
+    fn new(foreground: Color, background: Color) -> ColorCode {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
+
+struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode,
+}
+
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+
+use volatile::Volatile;
+
+// Volatile type means must use the write method to write to Buffer
+struct Buffer {
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+pub struct Writer {
+    column_position: usize,
+    color_code: ColorCode,
+    buffer: &'static mut Buffer, // Store ref to VGA buffer
+}
+
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line();
+                }
+
+                let row = BUFFER_HEIGHT - 1;
+                let col = self.column_position;
+
+                let color_code = self.color_code;
+                // Use write method instead of assignment
+                self.buffer.chars[row][col].write(ScreenChar {
+                    ascii_character: byte,
+                    color_code,
+                });
+                self.column_position += 1;
+            }
+        }
+    }
+
+    fn new_line(&mut self) {/* TODO */}
+
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                // printable ASCII byte or newline
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                // not part of printable ASCII range
+                _ => self.write_byte(0xfe),
+            }
+
+        }
+    }
+}
+
+pub fn print_something() {
+    // VGA buffer accessible via memory-mapped I/O to 0xb8000
+    let mut writer = Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    };
+
+    writer.write_byte(b'H');
+    writer.write_string("ello ");
+    writer.write_string("Wörld!");
+}
